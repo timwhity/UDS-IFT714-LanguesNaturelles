07dbdca626ccbca936947a7fea9cd1d9d6965b96
Jeremi Levesque <jeremilevesque@hotmail.com>
2024-03-19 15:47:29 -0400

    Full Untested RobertaTrainer

--- trainers/roberta_trainer.py
+++ trainers/roberta_trainer.py
@@ -29,7 +29,7 @@ class RobertaTrainer(BaseTrainer):
 
             self.optimizer.zero_grad()
 
-            outputs = self.model(inputs)
+            outputs = self.model(inputs_ids, attention_masks=attention_masks)
             loss = self.loss_fn(outputs, targets)
 
             loss.backward()
@@ -45,9 +45,42 @@ class RobertaTrainer(BaseTrainer):
         return self.history["train"]
 
     
-    def validate(self):
-        print("Validating...")
+    def validate(self, test: bool = False):
+        hist_key = "valid"
+        dataloader = self.validloader
+        if test:
+            hist_key = "test"
+            dataloader = self.testloader
+
+        total_loss = 0.0
+        total_correct = 0
+
+        self.model.eval()
+
+        with torch.no_grad():
+            for inputs, targets in dataloader:
+                tokenized = self.tokenizer(inputs,
+                                        max_length=self.max_seq_length,
+                                        padding="max_length",
+                                        truncation=True,
+                                        return_attention_mask=True,
+                                        add_special_tokens=True)
+                inputs_ids = torch.tensor(tokenized["input_ids"], dtype=torch.long, device=self.device)
+                attention_masks = torch.tensor(tokenized["attention_mask"], dtype=torch.long)
+                targets = targets.to(self.device)
+
+
+                outputs = self.model(inputs_ids, attention_masks=attention_masks)
+                loss = self.loss_fn(outputs, targets)
+
+                total_loss += loss.item()
+                total_correct += (outputs == targets).sum().item()
+        
+        self.history[hist_key]["loss"].append(total_loss / len(self.validloader))
+        self.history[hist_key]["accuracy"].append(total_correct / len(self.validloader.dataset))
+
+        return self.history[hist_key]
 
     
     def test(self):
-        print("Testing...")
+        return self.validate(test=True)
