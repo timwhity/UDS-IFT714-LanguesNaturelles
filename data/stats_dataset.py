4b3d3f51bb68c4c35f403304e98b97ce043e1d7f
tymitri <tiwhity@laposte.net>
2024-04-06 19:12:20 -0400

    Improved stats

--- data/stats_dataset.py
+++ data/stats_dataset.py
@@ -1,31 +1,72 @@
 import argparse
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+# import matplotlib.pyplot as plt
+# import seaborn as sns
 from pathlib import Path
 
-sns.set_style("darkgrid")
+# sns.set_style("darkgrid")
 
 def get_dataset_stats(dataset):
+    len_moy = 0
+    len_max = 0
+    len_min = 1000000
+    nb_benign = 0
+    nb_malicious = 0
+    for i in range(len(dataset)):
+        len_moy += len(dataset["url"][i])
+        len_max = max(len_max, len(dataset["url"][i]))
+        len_min = min(len_min, len(dataset["url"][i]))
+        if dataset["result"][i] == 0:
+            nb_benign += 1
+        else:
+            nb_malicious += 1
+    len_moy /= len(dataset)
     return {
-        "num_samples": len(dataset),
-        "num_benign": len(dataset[dataset["result"] == 0]),
-        "num_malicious": len(dataset[dataset["result"] == 1]),
+        "Nombre total d'url": len(dataset),
+        "Nombre d'url bénines": nb_benign,
+        "Nombres d'url malicieuses": nb_malicious,
+        "Longueur moyenne": len_moy,
+        "Longueur maximum": len_max,
+        "Longueur mininimum": len_min
     }
 
-def plot_dataset_stats(stats, save_dir=None):
+# def plot_dataset_stats(stats, save_dir=None):
     
-    # Plot a pie chart of the number of benign and malicious samples
-    fig, ax = plt.subplots()
-    ax.pie([stats["num_benign"], stats["num_malicious"]], labels=["Benign ({})".format(stats["num_benign"]), "Malicious ({})".format(stats["num_malicious"])], autopct="%1.1f%%")
-    ax.set_title(f"Dataset Stats\nNum Samples: {stats['num_samples']}")
+#     # Plot a pie chart of the number of benign and malicious samples
+#     fig, ax = plt.subplots()
+#     ax.pie([stats["num_benign"], stats["num_malicious"]], labels=["Benign ({})".format(stats["num_benign"]), "Malicious ({})".format(stats["num_malicious"])], autopct="%1.1f%%")
+#     ax.set_title(f"Dataset Stats\nNum Samples: {stats['num_samples']}")
 
+
+#     if save_dir is not None:
+#         save_dir = Path(save_dir) / "stats.png"
+#         plt.savefig(save_dir)
+#     else:
+#         plt.show()
+
+def save_dataset_stats(stats, save_dir, latex=False):
+    text = ""
+    if latex:
+        text += "\\begin{table}[]\n"
+        text += "\\begin{tabular}{|l|l|}\n"
+        text += "\\hline\n"
+        text += "Statistique & Valeur \\\\ \n"
+        text += "\\hline\n"
+        for key, value in stats.items():
+            text += f"{key} & {value} \\\\ \n"
+        text += "\\hline\n"
+        text += "\\end{tabular}\n"
+        text += "\\end{table}\n"
+    else:
+        for key, value in stats.items():
+            text += f"{key}: {value}\n"
 
     if save_dir is not None:
-        save_dir = Path(save_dir) / "stats.png"
-        plt.savefig(save_dir)
+        save_dir = Path(save_dir) / "stats.txt"
+        with open(save_dir, "w") as f:
+            f.write(text)
     else:
-        plt.show()
+        print(text)
 
 
 def main(args):
@@ -33,12 +74,14 @@ def main(args):
     # Load the dataset
     dataset = pd.read_csv(args.dataset)
     stats = get_dataset_stats(dataset)
-    plot_dataset_stats(stats, args.save_dir)
+    # plot_dataset_stats(stats, args.save_dir)
+    save_dataset_stats(stats, args.save_dir, args.latex)
 
 
 if '__main__' == __name__:
     parser = argparse.ArgumentParser(description="Train a model on a dataset.")
     parser.add_argument("dataset", type=str, help="The dataset to train on.")
     parser.add_argument("--save_dir", type=str, help="The directory to save the trained model.")
+    parser.add_argument("--latex", action="store_true", help="Whether to save the stats in a latex table format.")
     parser.add_argument("--seed", type=int, default=42, help="The random seed to use.") 
     main(parser.parse_args())
