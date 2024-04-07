a5ce97d49606bda14c36d0ad5d7db953e691ee93
tymitri <tiwhity@laposte.net>
2024-04-06 20:53:21 -0400

    Début cnn !

--- models/cnn.py
+++ models/cnn.py
@@ -1,2 +1,24 @@
 import torch.nn as nn
 
+
+class CNNUrl(nn.Module):
+
+	def __init__(self) -> None:
+		super().__init__()
+
+		# Embedding : (nb_char_dic , max_seq_length) -> (128, 16) ???
+		# Conv 2D ? 1D ? 
+		# Maxpooling
+		# Conv 2D ? 1D ?
+		# Maxpooling
+		# FC -> 1
+  
+
+		# TODO
+  
+
+	def forward(self, x):
+		# TODO
+		pass
+
+		
---
+++ trainers/cnn_trainer.py
@@ -0,0 +1,136 @@
+from .base_trainer import BaseTrainer
+from tqdm import tqdm
+from numpy import array
+from typing import List
+import torch
+
+class CNNTrainer(BaseTrainer):
+	def __init__(self, experiment, model, tokenizer, loss_fn, optimizer, scheduler, trainloader, validloader, testloader, classes, device, limit=None, max_seq_length=256) -> None:
+		super().__init__(experiment, "cnn", model, tokenizer, loss_fn, optimizer, scheduler, trainloader, validloader, testloader, classes, device, limit)
+		self.max_seq_length = max_seq_length
+		self.char_dic = "abcdefghijklmnopqrstuvwxyz0123456789-_.~!*';:@&=+$,/?#"
+		self.nb_char_dic = len(self.char_dic)
+
+	
+	def tokenizer(self, url: str) -> array:
+		"""
+		Input : string of the url
+		Output : numpy array (max_seq_length, nb_char_dic)
+		"""
+		sparse_vector = array((self.max_seq_length, self.nb_char_dic))
+		nb_accepted_characters = 0
+		for i, c in enumerate(url):
+			if c in self.char_dic:
+				sparse_vector[nb_accepted_characters, self.char_dic.index(c)] = 1
+				nb_accepted_characters += 1
+			elif c.lower() in self.char_dic:
+				sparse_vector[nb_accepted_characters, self.char_dic.index(c.lower())] = 1
+				nb_accepted_characters += 1
+			if nb_accepted_characters == self.max_seq_length:
+				break
+		return sparse_vector
+	
+	def tokenizer_batch(self, urls: List[str]) -> array:
+		"""
+		Input : list of urls
+		Output : numpy array (len(urls), max_seq_length, nb_char_dic)
+		"""
+		return array([self.tokenizer(url) for url in urls])
+	
+	def predict(self, texts: List[str]):
+		return self.model(self.tokenizer_batch(texts), probs=True)
+	
+	def train(self, eval_each: int = 0, epoch_title: str = "Epoch"):
+		self.model.train()
+
+		total_loss = 0.0
+		total_correct = 0
+		count_examples = 0
+		count_batches = 0
+
+		for batch_index, (inputs, targets) in enumerate(tqdm(self.trainloader, desc=epoch_title)):
+			inputs = self.tokenizer_batch(inputs)
+			inputs = inputs.to(self.device)
+			targets = targets.to(self.device)
+
+			self.optimizer.zero_grad()
+
+			outputs = self.model(inputs)
+			preds = outputs.argmax(dim=1)
+			loss = self.loss_fn(outputs, targets)
+
+			loss.backward()
+			self.optimizer.step()
+			self.scheduler.step()
+
+			total_loss += loss.item()
+			total_correct += (preds == targets).sum().item()
+			count_examples += len(targets)
+			count_batches += 1
+
+			avg_loss = total_loss / count_batches
+			accuracy = total_correct / count_examples
+			self.metrics.update_train_metrics(avg_loss, accuracy, 0.0)
+
+			if (eval_each > 0) and (batch_index % eval_each == 0):
+				self.validate()
+			if self.limit and (batch_index >= self.limit):
+				break
+
+		return self.metrics.get_metrics("train")
+	
+	def validate(self, test: bool = False):
+		desc = "valid"
+		dataloader = self.validloader
+		if test:
+			desc = "test"
+			dataloader = self.testloader
+			TP = 0
+			TN = 0
+			FP = 0
+			FN = 0
+
+		total_loss = 0.0
+		total_correct = 0
+		count_examples = 0
+		count_batches = 0
+		
+		self.model.eval()
+
+		with torch.no_grad():
+			for batch_index, (inputs, targets) in enumerate(tqdm(dataloader, desc=desc)):
+				inputs = self.tokenizer_batch(inputs)
+				inputs = inputs.to(self.device)
+				targets = targets.to(self.device)
+
+				outputs = self.model(inputs)
+				preds = outputs.argmax(dim=1)
+				loss = self.loss_fn(outputs, targets)
+
+				total_loss += loss.item()
+				total_correct += (preds == targets).sum().item()
+				count_examples += len(targets)
+				count_batches += 1
+
+				if test:
+					TP += ((preds == 1) & (targets == 1)).sum().item()
+					TN += ((preds == 0) & (targets == 0)).sum().item()
+					FP += ((preds == 1) & (targets == 0)).sum().item()
+					FN += ((preds == 0) & (targets == 1)).sum().item()
+
+		avg_loss = total_loss / count_batches
+		accuracy = total_correct / count_examples
+
+		if test:
+			self.metrics.update_test_metrics(avg_loss, accuracy, TP, TN, FP, FN)
+		else:
+			self.metrics.update_valid_metrics(avg_loss, accuracy)
+
+
+	def test(self):
+		return self.validate(test=True)
+
+
+
+
+		
