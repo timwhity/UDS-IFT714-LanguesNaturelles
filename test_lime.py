1aba031ecbefa88f71beaf24b68bdc87cb3bb263
Jeremi Levesque <jeremilevesque@hotmail.com>
2024-04-09 14:11:57 -0400

    Uncomment URLs chosen

--- test_lime.py
+++ test_lime.py
@@ -14,20 +14,18 @@ from utils.utils import add_default_arguments
 from interpretability.url_explainer import UrlExplainer
 from model_utils import load_model, load_config_data
 
-# URLS_TO_EXPLAIN = [
-#     # Benign
-#     "https://www.google.com",
-#     "https://jeremilevesque.com",
-#     "https://www.youtube.com/watch?v=j-t91Bzo8cE",
-#     "paypal.com.cgi.bin.webscr.cmd.flow.session.lohzumu98pjkwkwudgtj3ie6btlub.online775885d80a13c0db1f8e263663d3faee8d43b1bb6ca3ufquez.login.efzg5epaloginuk.uzmandoktorum.com/",
+URLS_TO_EXPLAIN = [
+    # Benign
+    "https://www.google.com",
+    "https://jeremilevesque.com",
+    "https://www.youtube.com/watch?v=j-t91Bzo8cE",
+    "paypal.com.cgi.bin.webscr.cmd.flow.session.lohzumu98pjkwkwudgtj3ie6btlub.online775885d80a13c0db1f8e263663d3faee8d43b1bb6ca3ufquez.login.efzg5epaloginuk.uzmandoktorum.com/",
     
-#     # Malicious
-#     "www.angelfire.com/az2/svtool/index.html",
-#     "http://fywuw8ar.myutilitydomain.com/file/1fb4e1a4e2a2dadd0334d0dc641877d7/",
-#     "http://cheaproomsvalencia.com/Paypal/Support/ID-NUMB629/myaccount/signin"
-# ]
-
-URLS_TO_EXPLAIN = ["google.com"]
+    # Malicious
+    "www.angelfire.com/az2/svtool/index.html",
+    "http://fywuw8ar.myutilitydomain.com/file/1fb4e1a4e2a2dadd0334d0dc641877d7/",
+    "http://cheaproomsvalencia.com/Paypal/Support/ID-NUMB629/myaccount/signin"
+]
 
 def main(args):
     experiment_name = args.experiment_name
