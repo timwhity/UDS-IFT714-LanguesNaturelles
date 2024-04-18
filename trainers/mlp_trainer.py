e4722aeda0f6b489a83d578b65971e2c423f1948
Jeremi Levesque <jeremilevesque@hotmail.com>
2024-04-18 16:43:00 -0400

    Fix MLP

--- trainers/mlp_trainer.py
+++ trainers/mlp_trainer.py
@@ -11,12 +11,13 @@ class MLPTrainer(BaseTrainer):
     def __init__(self, experiment, model, tokenizer, loss_fn, optimizer, scheduler, splits_directory, batch_size, device, limit=None, max_seq_length=512):
         super().__init__(experiment, "mlp", model, tokenizer, loss_fn, optimizer, scheduler, splits_directory, batch_size, device, limit)
     
+
     def predict(self, texts):
         self.model.eval()
 
         batch_preds = []
         for text_batch in tqdm(self.batch(texts, n=32), total=len(texts)//32 + 1):
-            tokenized = self.tokenizer.tokenize(text_batch)
+            data = self.tokenizer.tokenize(text_batch)
             data, target = torch.tensor(data, dtype=torch.float32, device=self.device), torch.tensor(target, dtype=torch.float32, device=self.device)
             
             with torch.no_grad():
@@ -31,34 +32,105 @@ class MLPTrainer(BaseTrainer):
     
 
     def train(self, eval_each: int = 0, epoch_title: str = "Epoch"):
-        # Training logic
         self.model.train()
-        for batch_idx, (data, target) in enumerate(tqdm(self.trainloader, desc=epoch_title)):
+
+        total_loss = 0.0
+        total_correct = 0
+        count_examples = 0
+        count_batches = 0
+
+        for batch_index, (data, targets) in enumerate(tqdm(self.trainloader, desc=epoch_title)):
             data = self.tokenizer.tokenize(data)
-            data, target = torch.tensor(data, dtype=torch.float32, device=self.device), torch.tensor(target, dtype=torch.float32, device=self.device)
+            data = torch.tensor(data, dtype=torch.float32, device=self.device)
+            targets = targets.float().to(self.device)
             self.optimizer.zero_grad()
-            output = self.model(data)
-            loss = self.loss_fn(output, target)
+
+            outputs = self.model(data)
+            preds = (outputs > 0.5).long()
+
+            loss = self.loss_fn(outputs, targets)
             loss.backward()
+            
             self.optimizer.step()
+            self.scheduler.step()
+
+            total_loss += loss.item()
+            total_correct += (preds == targets).sum().item()
+
+            # Keep the count of examples and batches so the metrics are accurate.
+            count_examples += len(targets)
+            count_batches += 1
+
+            lr = self.optimizer.param_groups[0]["lr"]
+            avg_loss = total_loss / count_batches
+            accuracy = total_correct / count_examples
+            self.metrics.update_train_metrics(avg_loss, accuracy, lr)
+
+            if (eval_each > 0) and (batch_index % eval_each == 0):
+                self.validate()
+
+            if batch_index > (len(self.trainloader) // 10) and accuracy > 0.995:
+                break
+
+            if self.limit and (batch_index >= self.limit): # Break prematurely for debugging on CPU or poor GPU
+                break
+
+        self.test()
+
+        return self.metrics.get_metrics("train")
 
     def validate(self, test: bool = False):
-        # Validation logic
-        self.model.eval()
-        val_loss = 0
-        correct = 0
-        with torch.no_grad():
-            for data, target in self.validloader:
-                data = self.tokenizer.tokenize(data)
-                data, target = torch.tensor(data, dtype=torch.float32, device=self.device), torch.tensor(target, dtype=torch.float32, device=self.device)
-                output = self.model(data)
-                val_loss += self.loss_fn(output, target).item()  # sum up batch loss
-                pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-                correct += pred.eq(target.view_as(pred)).sum().item()
+        desc = "valid"
+        dataloader = self.validloader
+        if test:
+            desc = "test"
+            dataloader = self.testloader
+            TP = 0 # Confusion matrix values counters
+            TN = 0
+            FP = 0
+            FN = 0
 
-        val_loss /= len(self.validloader.dataset)
-        accuracy = 100. * correct / len(self.validloader.dataset)
-        return val_loss, accuracy
+        total_loss = 0.0
+        total_correct = 0
+        count_examples = 0
+        count_batches = 0
+
+        self.model.eval()
+
+        pbar = tqdm(range(len(dataloader)), desc=desc)
+
+        with torch.no_grad():
+            for batch_index, (data, targets) in enumerate(dataloader):
+                data = self.tokenizer.tokenize(data)
+                data = torch.tensor(data, dtype=torch.float32, device=self.device)
+                targets = targets.float().to(self.device)
+                
+                outputs = self.model(data)
+                preds = (outputs > 0.5).long()
+                loss = self.loss_fn(outputs, targets)
+
+                if test:
+                    TP += ((preds == 1) & (targets == 1)).sum().item()
+                    TN += ((preds == 0) & (targets == 0)).sum().item()
+                    FP += ((preds == 1) & (targets == 0)).sum().item()
+                    FN += ((preds == 0) & (targets == 1)).sum().item()
+
+                pbar.update()
+                total_loss += loss.item()
+                total_correct += (preds == targets).sum().item()
+                count_examples += len(targets)
+                count_batches += 1
+
+                if self.limit and (batch_index >= self.limit): # Break prematurely for debugging on CPU or poor GPU
+                    break
+
+        avg_loss = total_loss / count_batches
+        accuracy = total_correct / count_examples
+
+        if test:
+            return self.metrics.update_test_metrics(avg_loss, accuracy, TP, TN, FP, FN)
+        else:
+            return self.metrics.update_valid_metrics(avg_loss, accuracy)
 
     def test(self):
         return self.validate(test=True)
